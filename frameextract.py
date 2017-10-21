38038e4bacb47a3dd657c5eba6b80fbf91872d54
Harsh Tiku <tiku.harsh@gmail.com>
2017-10-21 09:35:04 +0530

    Add files via upload

---
+++ frameextract.py
@@ -0,0 +1,27 @@
+import os
+
+
+
+
+
+
+
+
+
+os.chdir('./train')
+os.mkdir('./outputs')
+videos=os.listdir()
+
+for video in videos[1:]:
+	path=os.path.realpath(video)
+	if os.path.isfile(path):
+		os.system( 'ffmpeg -i {} -r 1/20  ./outputs/%03d.jpg'.format(path))
+
+	images=os.listdir('./outputs')
+	for image in images:
+		store(image)
+
+
+
+#imagestore=np.array(imagestore)
+#np.save('imagestore.npy',imagestore)
